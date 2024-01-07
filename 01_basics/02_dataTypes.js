05dc3117fd509701e783e2ceec23f6e29e3c335f
krishna8399 <77582342+krishna8399@users.noreply.github.com>
2024-01-07 19:02:38 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as new version
+
+// alert("hello") // we are using nodejs, not browser
+
+let name = "krishna" // string >> ""
+let age = 24 // number >> 2 to power 53
+let isLoogedIn = false// boolean >> true/false
+let state = null //NULL >> standalone value
+// undefined >> 
+// symbol >> unique 
+
+// object
+
+console.log(typeof "krishna") // to know the data type 
+console.log(typeof NULL) // object
+console.log(typeof undefined) // undefined
