33ed3c0308aa1c443f9b858211e503e8df94baa7
KRISHNA SINGH <krishnasingh08031999@gmail.com>
2024-01-12 17:53:39 +0000

    datatype conversion

---
+++ 01_basics/03_typeconv.js
@@ -0,0 +1,18 @@
+/*let score = 33
+let score1 = "33"
+
+console.log(typeof score);
+console.log(typeof (score1));
+
+let valueInNumber = Number(score); 
+console.log(typeof valueInNumber); 
+
+// "33" => 33
+// "33abc" => NaN not a number
+// true => 1; faolse => 0
+*/
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
