8e22bdac1c10c13e73a88d51452da180faa24d6f
KRISHNA SINGH <krishnasingh08031999@gmail.com>
2024-01-13 06:08:37 +0000

    type convrsn

--- 01_basics/03_typeconv.js
+++ 01_basics/03_typeconv.js
@@ -10,9 +10,16 @@ console.log(typeof valueInNumber);
 // "33" => 33
 // "33abc" => NaN not a number
 // true => 1; faolse => 0
-*/
+//
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 console.log(booleanIsLoggedIn)
+*/
+
+//################# OPERATIONS #########################
+let value = 3;
+let negalue = -value;
+//console.log(negalue); //-3
+
