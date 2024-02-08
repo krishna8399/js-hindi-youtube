08b7ae66241779474fdfb2a8e250478fbcb6b17b
KRISHNA SINGH <krishnasingh08031999@gmail.com>
2024-02-08 13:58:17 +0000

    dowhile

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// do while 
+
+let index = 0
+while (index <= 10) {
+    console.log(`value if index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["superman","spiderman","batman"]
+
+let arr = 0 
+while (arr < myArray.length) {
+   console.log(`value is ${myArray[arr]}`);
+   arr = arr + 1
+}
+
+// do while
+score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
