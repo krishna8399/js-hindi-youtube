13e7cfecca34cb6524ccfeefd3e4aa58202978c8
KRISHNA SINGH <krishnasingh08031999@gmail.com>
2024-02-09 03:08:47 +0000

    filter

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -1,2 +1,13 @@
-const coding = ["js","ruby","java"]
+const myNums = [1,2,3,4,5,6,7,8,9,10] 
+
+// filter 
+
+// const newNum = myNums.filter( (num) => num > 4)
+// OR
+const newNum = myNums.filter( (num) => {
+    return num > 4})
+
  
+
+
+console.log(newNum);
