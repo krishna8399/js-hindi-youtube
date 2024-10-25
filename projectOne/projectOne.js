ee8bccf195e606011c44edfd7412abd11a29cc3c
KRISHNA SINGH <krishnasingh08031999@gmail.com>
2024-10-25 11:43:37 +0000

    css done

--- projectOne/projectOne.js
+++ projectOne/projectOne.js
@@ -1,19 +1,21 @@
-const arr= [10,20,40,50,100]
+// const arr= [10,20,40,50,100]
 
-// for
+// // for
 
-for (let i = 0; i < arr.length; i++) {
-    console.log(`using for ${arr[i]}`);
-}
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(`using for ${arr[i]}`);
+// }
 
-//for of
+// //for of
 
-for (const element of arr) {
-    console.log(`using for of ${element}`); 
-}
+// for (const element of arr) {
+//     console.log(`using for of ${element}`); 
+// }
 
-// for each 
-//The forEach() method of Array instances executes a provided function once for each array element.
-arr.forEach(element => {
-    console.log(`using for each ${element}`);
-});
+// // for each 
+// //The forEach() method of Array instances executes a provided function once for each array element.
+// arr.forEach(element => {
+//     console.log(`using for each ${element}`);
+// });
+
+
